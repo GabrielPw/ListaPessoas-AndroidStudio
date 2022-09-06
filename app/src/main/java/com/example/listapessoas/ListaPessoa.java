199c43aeafa2a2a7a93bce58cc30eee0ba6f638c
GabrielPw <gxaavier352@gmail.com>
2022-09-06 12:33:43 -0300

    criando View 'listaPessoa'

---
+++ app/src/main/java/com/example/listapessoas/ListaPessoa.java
@@ -0,0 +1,16 @@
+package com.example.listapessoas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaPessoa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pessoa);
+    }
+
+    // Add. containers de info conforme pessoas são adicionadas na lista.
+}
